6c4d20f32137dfd8918d115f418d29b37f65b687
Chaitanya Bhardwaj <120612691+Coderninja23@users.noreply.github.com>
2023-04-26 12:04:12 +0530

    Add files via upload
    Added Sections

---
+++ src/Components/Body/Sections/Section_contact.jsx
@@ -0,0 +1,28 @@
+function Section_contact() {
+  return (
+    <section id="contact">
+      <div class="section-inner">
+        <h2>Contact me</h2>
+        <p>You can find me on:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/Coderninja23">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/c_bhardwaj04/">Instagram</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/chaitanya-bhardwaj-45109826b/">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+        <p>
+          Or, you can{" "}
+          <a href="mailto:22ec01004@iitbbs.ac.in">send me an email</a>
+        </p>
+      </div>
+    </section>
+  );
+}
+export default Section_contact;
