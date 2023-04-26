a562e3c85e035cef6fd72ba471d9642508ea0da1
Chaitanya Bhardwaj <120612691+Coderninja23@users.noreply.github.com>
2023-04-26 12:16:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Title from "./Components/Title";
 import Body from "./Components/Body/Body";
-import '../src/index.css'
+import "../src/index.css";
 import Header from "./Components/Body/Header";
 
 import Footer from "./Components/Body/Footer";
@@ -8,12 +8,13 @@ import Footer from "./Components/Body/Footer";
 function App() {
   return (
     <div>
-    <Title></Title>
+      <Title></Title>
 
-    <Header/>
-    <Body/>
-    <Footer/>
+      <Header />
 
+      <Body />
+
+      <Footer />
     </div>
   );
 }
