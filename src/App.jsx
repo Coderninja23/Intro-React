e8eedd7f6e3b62fd2f8c8096c6e155d1e8748f2d
Chaitanya Bhardwaj <120612691+Coderninja23@users.noreply.github.com>
2023-04-26 12:00:55 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Title from "./Components/Title";
+import Body from "./Components/Body/Body";
+import '../src/index.css'
+import Header from "./Components/Body/Header";
+
+import Footer from "./Components/Body/Footer";
+
+function App() {
+  return (
+    <div>
+    <Title></Title>
+
+    <Header/>
+    <Body/>
+    <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
